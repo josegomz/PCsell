2832b6ce6f9faa856aebbc7a64f77651cfb67362
jose benito <josegomz.sa17@gmail.com>
2020-01-17 01:16:18 -0600

    ultimos cambios

--- src/java/resources_sql/PCsell.sql
+++ src/java/resources_sql/PCsell.sql
@@ -28,15 +28,15 @@ create table usuario(
 create table marcaprocesador(
 	id bigserial not null,
 	nombre text not null,
-	imagen bigserial not null,
+	idimagen bigserial not null,
 	constraint pk_procesador_marca primary key(id),
-	constraint fk_mp_imagen foreign key(imagen) references imagen(id) on delete cascade on update cascade
+	constraint fk_mp_imagen foreign key(idimagen) references imagen(id) on delete cascade on update cascade
 );
 create table procesador(
 	id bigserial not null,
-	marca bigserial not null,
+	idmarca bigserial not null,
 	modelo text not null,
-	imagen bigserial not null,
+	idimagen bigserial not null,
 	nucleos integer not null,
 	hilosdeprocesamiento integer,
 	frecuenciabase decimal,
@@ -45,15 +45,15 @@ create table procesador(
 	cantidades integer,
 	descripcion text,
 	constraint pk_procesador primary key(id),
-	constraint fk_procesador_marca foreign key(marca) references marcaprocesador(id) on delete cascade on update cascade,
-	constraint fk_procesador_imagen foreign key(imagen) references imagen(id) on delete cascade on update cascade
+	constraint fk_procesador_marca foreign key(idmarca) references marcaprocesador(id) on delete cascade on update cascade,
+	constraint fk_procesador_imagen foreign key(idimagen) references imagen(id) on delete cascade on update cascade
 );
 
 create table disipadorcalor(
 	id bigserial not null,
 	marca text not null,
 	modelo text not null,
-	imagen bigserial not null,
+	idimagen bigserial not null,
 	tdp decimal,
 	tubostermicos integer,
 	ancho decimal,
@@ -62,7 +62,7 @@ create table disipadorcalor(
 	cantidad integer,
 	descripcion text,
 	constraint pk_disipador primary key(id),
-	constraint fk_disipador_imagen foreign key(imagen) references imagen(id) on delete cascade on update cascade
+	constraint fk_disipador_imagen foreign key(idimagen) references imagen(id) on delete cascade on update cascade
 );
 
 create table tamaniotm(
@@ -75,25 +75,25 @@ create table tarjetamadre(
 	id bigserial not null,
 	marca text not null,
 	modelo text not null,
-	imagen bigserial not null,
+	idimagen bigserial not null,
 	ram text not null,
 	almacenamiento text not null,
 	precio decimal not null,
 	cantidad integer,
 	descripcion text,
-	tamanio bigserial not null,
-	procesador bigserial not null,
+	idtamanio bigserial not null,
+	idprocesador bigserial not null,
 	constraint pk_tarjeta_madre primary key(id),
-	constraint fk_tamanio_tm foreign key(tamanio) references tamaniotm(id) on delete cascade on update cascade,
-	constraint fk_tarjeta_procesador foreign key(procesador) references marcaprocesador(id) on delete cascade on update cascade,
-	constraint fk_tm_imagen foreign key(imagen) references imagen(id) on delete cascade on update cascade
+	constraint fk_tamanio_tm foreign key(idtamanio) references tamaniotm(id) on delete cascade on update cascade,
+	constraint fk_tarjeta_procesador foreign key(idprocesador) references marcaprocesador(id) on delete cascade on update cascade,
+	constraint fk_tm_imagen foreign key(idimagen) references imagen(id) on delete cascade on update cascade
 );
 
 create table memoriaram(
 	id bigserial not null,
 	marca text not null,
 	modelo text not null,
-	imagen bigserial not null,
+	idimagen bigserial not null,
 	velocidad decimal not null,
 	capacidad integer not null,
 	precio decimal not null,
@@ -101,14 +101,14 @@ create table memoriaram(
 	cantidad integer,
 	descripcion text,
 	constraint pk_memoria_ram primary key(id),
-	constraint fk_ram_imagen foreign key(imagen) references imagen(id) on delete cascade on update cascade
+	constraint fk_ram_imagen foreign key(idimagen) references imagen(id) on delete cascade on update cascade
 );
 
 create table discoduro(
 	id bigserial,
 	marca text not null,
 	modelo text not null,
-	imagen bigserial not null,
+	idimagen bigserial not null,
 	factordeforma text not null,
 	rpm integer not null,
 	interfaz text not null,
@@ -117,14 +117,14 @@ create table discoduro(
 	precio decimal not null,
 	descripcion text,
 	constraint pk_disco_duro primary key(id),
-	constraint fk_disco_imagen foreign key(imagen) references imagen(id) on delete cascade on update cascade
+	constraint fk_disco_imagen foreign key(idimagen) references imagen(id) on delete cascade on update cascade
 );
 
 create table tarjetavideo(
 	id bigserial not null,
 	marca text not null,
 	modelo text not null,
-	imagen bigserial not null,
+	idimagen bigserial not null,
 	vram text not null,
 	ram integer not null,
 	conexion text,
@@ -132,59 +132,59 @@ create table tarjetavideo(
 	precio decimal not null,
 	cantidad integer,
 	constraint pk_tarjeta_video primary key(id),
-	constraint fk_tv_imagen foreign key(imagen) references imagen(id) on delete cascade on update cascade
+	constraint fk_tv_imagen foreign key(idimagen) references imagen(id) on delete cascade on update cascade
 );
 
 create table gabinete(
 	id bigserial not null,
 	marca text not null,
 	modelo text not null,
-	imagen bigserial not null,
+	idimagen bigserial not null,
 	tamaño text not null,
-	tamaniotm bigserial not null,
+	idtamaniotm bigserial not null,
 	descripcion text,
 	precio decimal not null,
 	cantidad integer,
 	constraint pk_gabinete primary key(id),
-	constraint fk_tamanio_gabinete foreign key(tamaniotm) references tamaniotm(id) on delete cascade on update cascade,
-	constraint fk_gabinete_imagen foreign key(imagen) references imagen(id) on delete cascade on update cascade
+	constraint fk_tamanio_gabinete foreign key(idtamaniotm) references tamaniotm(id) on delete cascade on update cascade,
+	constraint fk_gabinete_imagen foreign key(idimagen) references imagen(id) on delete cascade on update cascade
 );
 
 create table fuentepoder(
 	id bigserial not null,
 	marca text not null,
 	modelo text not null,
-	imagen bigserial not null,
-	tamaniotm bigserial not null,
+	idimagen bigserial not null,
+	idtamaniotm bigserial not null,
 	precio decimal not null,
 	cantidad integer,
 	descripcion text,
 	constraint pk_fuente primary key(id),
-	constraint fk_fuente_tm foreign key(tamaniotm) references tamaniotm(id) on delete cascade on update cascade,
-	constraint fk_fuentepoder_imagen foreign key(imagen) references imagen(id) on delete cascade on update cascade
+	constraint fk_fuente_tm foreign key(idtamaniotm) references tamaniotm(id) on delete cascade on update cascade,
+	constraint fk_fuentepoder_imagen foreign key(idimagen) references imagen(id) on delete cascade on update cascade
 );
 
 create table procesadordisipador(
         id bigserial not null,
-	procesador bigserial not null,
-	disipador bigserial not null,
+	idprocesador bigserial not null,
+	iddisipador bigserial not null,
 	constraint pk_procesador_disipador primary key(id),
-	constraint fk_procesador foreign key(procesador) references marcaprocesador(id) on delete cascade on update cascade,
-	constraint fk_disipador foreign key(disipador) references disipadorcalor(id) on delete cascade on update cascade
+	constraint fk_procesador foreign key(idprocesador) references marcaprocesador(id) on delete cascade on update cascade,
+	constraint fk_disipador foreign key(iddisipador) references disipadorcalor(id) on delete cascade on update cascade
 );
 
 create table computadora(
 	id bigserial not null,
-	procesador bigserial not null,
-	disipador bigserial not null,
-	tarjetamadre bigserial not null,
-	memoriaram bigserial not null,
-	memoriaramadicional bigserial not null,
-	discoduro bigserial not null,
-	discoduroadicional bigserial not null,
-	tarjetavideo bigserial not null,
-	gabinete bigserial not null,
-	fuentepoder bigserial not null,
+	idprocesador bigserial not null,
+	iddisipador bigserial not null,
+	idtarjetamadre bigserial not null,
+	idmemoriaram bigserial not null,
+	idmemoriaramadicional bigserial not null,
+	iddiscoduro bigserial not null,
+	iddiscoduroadicional bigserial not null,
+	idtarjetavideo bigserial not null,
+	idgabinete bigserial not null,
+	idfuentepoder bigserial not null,
 	costototal decimal not null,
 	constraint pk_computadora primary key(id),
 	constraint fk_computador_procesador foreign key(procesador) references procesador(id) on delete cascade on update cascade,
